125be24d43fd63bd9aaafd01132f301f9e6ef27c
saloni-bloopark <saloni-bloopark@github.com>
2021-04-22 09:53:38 +0200

    Create food_dishtype.py
    New Dish Type works and Employee Type suggestion

---
+++ bloopark-addons/bp_food/models/food_dishtype.py
@@ -0,0 +1,23 @@
+import time
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+from odoo import models, fields, api
+
+class DishType(models.Model):
+    _name = "food.dishtype"
+    _description = "describes the different types of dishes that can be added"
+
+    newdish = fields.Char(string="New Dish Name", required=True)
+    dish_ingredients = fields.Char(string="Specify the dish ingredients")
+    dish_type = fields.Selection(string='Dishes Type Veg or Non-Veg', selection=[
+        ('veg', 'Vegetarian'),
+        ('nonveg', 'Non-Vegetarian'),
+    ])
+    dish_comments = fields.Char("Add Comments about Dish")
+
+
+
