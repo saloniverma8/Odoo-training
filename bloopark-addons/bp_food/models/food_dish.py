225823e4d0a008e050f02e31ada1682efd75e1da
saloni-bloopark <saloni-bloopark@github.com>
2021-04-22 11:32:48 +0200

    Update food_dish.py
    Tables and capacity working

--- bloopark-addons/bp_food/models/food_dish.py
+++ bloopark-addons/bp_food/models/food_dish.py
@@ -4,29 +4,29 @@ from odoo.osv import expression
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
-class FoodDish(models.Model):
-    _name = "food.dish"
+class FoodDishAvail(models.Model):
+    _name = "food.dishavailable"
     _description = "Dishes Offered"
 
-    dishes = fields.Selection(string='Dishes Offered', selection=[
+    dishesavail = fields.Selection(string='Dishes Offered', selection=[
         ('shahi', 'Shahi Paneer'),
         ('chicken', 'Butter Chicken'),
     ])
 
-    shahipaneeringredients = fields.Selection(string='Shahi Paneer Ingredients', selection=[
-        ('Tomatoes', 'Tomatoes 100 gms'),
-        ('Onions', 'Onions 70 gms'),
-        ('spices', 'Indian Spices 2 gms'),
-        ('cottagecheese', 'Cottage Cheese 60 gms'),
-        ('creamsahne', 'Cream/Sahne 15 gms'),
-    ], default='1')
-
-    chickeningredients = fields.Selection(string='Butter Chicken Ingredients', selection=[
-        ('Chicken', 'Chicken Breast 200 gms'),
-        ('ispices', ' Indian Spices 5 gms'),
-        ('preservedmarinate', ' Preserved Marinate 100 gms'),
-        ('garnishing', 'Garnishing 10 gms'),
-        ('butter', 'Butter 30 gms'),
+    # shahipaneeringredients = fields.Selection(string='Shahi Paneer Ingredients', selection=[
+    #     ('Tomatoes', 'Tomatoes 100 gms'),
+    #     ('Onions', 'Onions 70 gms'),
+    #     ('spices', 'Indian Spices 2 gms'),
+    #     ('cottagecheese', 'Cottage Cheese 60 gms'),
+    #     ('creamsahne', 'Cream/Sahne 15 gms'),
+    # ], default='1')
+    #
+    # chickeningredients = fields.Selection(string='Butter Chicken Ingredients', selection=[
+    #     ('Chicken', 'Chicken Breast 200 gms'),
+    #     ('ispices', ' Indian Spices 5 gms'),
+    #     ('preservedmarinate', ' Preserved Marinate 100 gms'),
+    #     ('garnishing', 'Garnishing 10 gms'),
+    #     ('butter', 'Butter 30 gms'),
     ])
     # curry_ingredients = fields.Selection(string='Butter Chicken Ingredients', selection=[
     #     ('Curd/Yogurt', '100 gms'),
@@ -50,4 +50,3 @@ class FoodDish(models.Model):
     #     ('Orders', '100 gms'),
     # ])
 
-    new_dish = fields.Char(string='Add Dish Name')
