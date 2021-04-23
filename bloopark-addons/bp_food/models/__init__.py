369c4728b0cf856fb425cb1544653f915b2195bf
saloni-bloopark <saloni-bloopark@github.com>
2021-04-23 09:42:21 +0200

    Update __init__.py
    Updating init

--- bloopark-addons/bp_food/models/__init__.py
+++ bloopark-addons/bp_food/models/__init__.py
@@ -2,8 +2,8 @@
 
 from . import food_customer
 from . import food_employees
-from . import food_employeestype
-from . import food_dishtype
-# from . import food_dishavailable
+from . import food_menu
 from . import food_order
 from . import food_tables
+from . import food_dishtype
+from . import food_employeestype
