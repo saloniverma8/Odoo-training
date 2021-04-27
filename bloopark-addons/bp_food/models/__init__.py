edfd2f6d2fe8da02b67e5d898236cc0965accd69
saloni-bloopark <saloni-bloopark@github.com>
2021-04-27 11:56:01 +0200

    Removing bp_food addon

--- bloopark-addons/bp_food/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import food_customer
-from . import food_employees
-from . import food_menu
-from . import food_order
-from . import food_tables
-from . import food_dishtype
-from . import food_employeestype
