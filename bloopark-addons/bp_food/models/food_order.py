40499ac0446d219bfbf5f50515ad1ded72207b8e
saloni-bloopark <saloni-bloopark@github.com>
2021-05-10 14:35:44 +0200

    Order Menu UI Updated

--- bloopark-addons/bp_food/models/food_order.py
+++ bloopark-addons/bp_food/models/food_order.py
@@ -14,39 +14,21 @@ class FoodOrder(models.Model):
     _description = "Table Details"
 
     # table = fields.One2many('food.tables', 'table', required=True, index=True)
-    table = fields.One2many('food.tables', 'table', required=True)
+    table = fields.Many2one('food.tables', 'Table No.', required=True)
     capacity = fields.Integer("Capacity")
     order_number = fields.Integer("Order #")
     order_date = fields.Date(string='Order Date', required=True)
     # current_date = fields.Datetime(string='Current Date and Time')
-    order_customer = fields.Many2one('bp.food.customers', string ='Name of Customer')
+    order_customer = fields.Many2one('bp.food.customers', string='Name of Customer')
     order_dish = fields.Many2one('food.dishtype', string='Dish to be Ordered')
     order_server = fields.Many2one('food.employees', string='Server/Waiter Name')
     order_amount = fields.Integer(string='Bill Total')
     order_comments = fields.Char('Add comments here for customisation or Allergies')
 
 
-# Add Sale order line and computer fields for Bill here.
+@api.constrains('order_date')
 
-# @api.constrains('order_date')
-#
-#
-# def _check_order_date(self):
-#     if self.order_date <= self.current_date:
-#         raise ValidationError('End date must be after Start Date')
 
-#
-# table = fields.One2many('food.tables', 'table',
-#                                string='Table No. for Order')
-#     capacity = fields.fields.One2many('food.tables', 'capacity',
-#                                string='table capacity')
-#     order_number = fields.Integer("Order #", required=True, index=True)
-#     order_date = fields.Date(string='Order Date', required=True)
-#     # current_date = fields.Datetime(string='Current Date and Time')
-#     order_customer = fields.One2many('bp.food.customers', 'name',
-#                                string='Name of Customer')
-#     # order_dish = fields.Many2many('food.dish', 'dishes', string='Dish to be Ordered')
-#     order_server = fields.One2many('food.employees', 'name',
-#                                string='Name of Waiter/Server')
-#     order_amount = fields.Integer(string='Bill Total')
-#     order_comments = fields.Char('Add comments here for customisation or Allergies')
+def _check_order_date(self):
+    if self.order_date <= datetime.now():
+        raise ValidationError('Start date must be after today.')
