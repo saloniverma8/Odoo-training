45aef90d273f850e01018c213c6ef1ba8727dc2b
saloni-bloopark <saloni-bloopark@github.com>
2021-05-17 08:05:54 +0200

    food.dishtype corrected

--- bloopark-addons/bp_food/models/food_dishtype.py
+++ bloopark-addons/bp_food/models/food_dishtype.py
@@ -12,7 +12,7 @@ class DishType(models.Model):
     _name = "food.dishtype"
     _description = "describes the different types of dishes that can be added"
 
-    dishname = fields.Char(string="New Dish Name", required=True)
+    name = fields.Char(string="New Dish Name", required=True)
     dish_ingredients = fields.Char(string="Specify the Dish Ingredients")
     dish_type = fields.Selection([
         ('veg', 'Vegetarian'),
